e3b43e94b99fe459de55fcd095c4b215c5a88c9b
StylexTV <marmo24391@gmail.com>
2022-07-21 17:18:48 +0200

    Update ArticleItem.vue

--- components/blog/layout/section/article/ArticleItem.vue
+++ components/blog/layout/section/article/ArticleItem.vue
@@ -1,5 +1,5 @@
 <template>
-  <PageLink class="w-1/2 h-full px-[16px] pb-[72px] flex flex-col" :to="{ name: 'blog-slug', params: { slug: article.slug } }">
+  <PageLink class="w-1/3 h-full px-[16px] pb-[72px] flex flex-col" :to="{ name: 'blog-slug', params: { slug: article.slug } }">
     <ArticleItemCover :article="article"/>
     <ArticleItemText :article="article"/>
   </PageLink>
