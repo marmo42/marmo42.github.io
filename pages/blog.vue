45409081a38221caa4cf5fbd36e572f310a79ec7
StylexTV <marmo24391@gmail.com>
2022-07-21 14:20:28 +0200

    Added blog pages

---
+++ pages/blog.vue
@@ -0,0 +1,14 @@
+<template>
+  <PageContainer>
+    <FullScreenSection></FullScreenSection>
+    <FullScreenSection></FullScreenSection>
+  </PageContainer>
+</template>
+
+<script>
+export default {
+  head: {
+    title: 'Marmo | Blog'
+  }
+}
+</script>
---
+++ pages/blog/_blog_article.vue
@@ -0,0 +1,13 @@
+<template>
+  <PageContainer>
+    <nuxt-content :document="article"/>
+  </PageContainer>
+</template>
+
+<script>
+export default {
+  head: {
+    title: 'Marmo | Blog | TITLE'
+  }
+}
+</script>
