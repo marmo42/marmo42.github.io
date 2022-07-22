f8465c45e7b952c62bed7a38402aa3b8265292e5
StylexTV <marmo24391@gmail.com>
2022-07-22 13:39:51 +0200

    Update PageHeader.vue

--- components/global/layout/PageHeader.vue
+++ components/global/layout/PageHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="absolute z-30 w-full bg-gray-dark flex justify-center">
+  <div class="absolute z-30 w-full flex justify-center">
     <div class="relative w-[1220px] h-[80px] flex justify-between">
       <HeaderLogo link="/"/>
       <HeaderNavbar/>
