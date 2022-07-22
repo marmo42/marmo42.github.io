d1ccfbfd5451df7f55a922a034664e4cecbb2dab
StylexTV <marmo24391@gmail.com>
2022-07-22 14:26:16 +0200

    Update LoaderIcon.vue

--- components/global/layout/loader/LoaderIcon.vue
+++ components/global/layout/loader/LoaderIcon.vue
@@ -1,3 +1,21 @@
 <template>
-  <Icon size="88px" name="brands/marmo"/>
+  <Icon size="88px" name="brands/marmo" v-anime="translate"/>
 </template>
+
+<script>
+  export default {
+    data() {
+      return {
+        translate: {
+          keyframes: [
+            {translateY: '-10%'},
+            {translateY: 0}
+          ],
+          easing: 'easeInOutQuad',
+          duration: 1000,
+          loop: true
+        }
+      }
+    }
+  }
+</script>
