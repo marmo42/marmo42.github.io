b55acaade5f134606de331abaff823442347ed43
StylexTV <marmo24391@gmail.com>
2022-07-24 15:11:18 +0200

    Added list component

--- components/article/layout/section/ArticleListSection.vue
+++ components/article/layout/section/ArticleListSection.vue
@@ -1,6 +1,8 @@
 <template>
-  <ContentSection class="pt-56 flex flex-wrap">
-    <ArticleItem v-for="(article, index) of articles" :key="article.slug" :article="article" :prominent="index == 0"/>
+  <ContentSection class="pt-56">
+    <List>
+      <ArticleItem v-for="(article, index) of articles" :key="article.slug" :article="article" :prominent="index == 0"/>
+    </List>
   </ContentSection>
 </template>
 
