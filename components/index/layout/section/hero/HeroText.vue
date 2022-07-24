84f6fe2bd8cd2c32e6de08f72b924fea711ed9c7
StylexTV <marmo24391@gmail.com>
2022-07-24 15:26:11 +0200

    Deleted unused components

--- components/index/layout/section/hero/HeroText.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="flex flex-col items-center">
-    <HeroTitle>Hi. I’m Seán.</HeroTitle>
-    <HeroTitle>A Designer.</HeroTitle>
-    <HeroSubtitle>I’m also a front-end developer and general doodler with a keen eye for creating engaging UI, bringing products to life.</HeroSubtitle>
-  </div>
-</template>
