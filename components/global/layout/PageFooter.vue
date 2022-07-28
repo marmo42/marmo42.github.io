c41692068f4e5bb9c51c89f38f83649b2334397f
StylexTV <marmo24391@gmail.com>
2022-07-28 13:17:36 +0200

    Update PageFooter.vue

--- components/global/layout/PageFooter.vue
+++ components/global/layout/PageFooter.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="w-full mt-52 pt-20 pb-16 bg-gray flex justify-center">
     <div class="w-[1180px] flex justify-between">
-      <HeaderSocials/>
+      <HeaderSocials class="opacity-50"/>
       <FooterBTT/>
     </div>
   </div>
