56a3b5cece2c0a47d9af6860f9eee467a20fc8df
StylexTV <marmo24391@gmail.com>
2022-07-28 13:06:59 +0200

    Update FooterBTT.vue

--- components/global/layout/footer/FooterBTT.vue
+++ components/global/layout/footer/FooterBTT.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="opacity-50 hover:opacity-80 flex items-center cursor-pointer" @click="scrollToTop">
+  <div class="opacity-50 hover:opacity-80 flex items-center transition cursor-pointer" @click="scrollToTop">
     <FooterBTTText>Back to top</FooterBTTText>
     <FooterBTTIcon/>
   </div>
