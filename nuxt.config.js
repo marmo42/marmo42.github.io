fee06f727efef75ddee1d0ba7f0e037cb0ab9e39
StylexTV <marmo24391@gmail.com>
2022-07-29 12:11:37 +0200

    Added auto import for components

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,39 +34,10 @@ export default {
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
-  components: {
-    dirs: [
-      '~/components',
-      '~/components/global',
-      '~/components/global/animation',
-      '~/components/global/text',
-      '~/components/global/text/sans',
-      '~/components/global/text/serif',
-      '~/components/global/link',
-      '~/components/global/icon',
-      '~/components/global/video',
-      '~/components/global/list',
-      '~/components/global/layout',
-      '~/components/global/layout/section',
-      '~/components/global/layout/section/content',
-      '~/components/global/layout/section/header',
-      '~/components/global/layout/section/header/title',
-      '~/components/index',
-      '~/components/index/layout',
-      '~/components/index/layout/section',
-      '~/components/about',
-      '~/components/about/layout',
-      '~/components/about/layout/section',
-      '~/components/article',
-      '~/components/article/layout',
-      '~/components/article/layout/section',
-      '~/components/article/layout/section/content',
-      '~/components/article/layout/section/content/author',
-      '~/components/article/layout/section/list',
-      '~/components/article/layout/section/list/item',
-      '~/components/article/layout/section/list/item/text'
-    ]
-  },
+  components: [{
+    path: '~/components',
+    pathPrefix: false
+  }],
 
   loading: '~/components/global/layout/PageLoader.vue',
 
