3026c9cc36fe45bbe4f8e5cc460f39074e1cc559
StylexTV <stylextv42@gmail.com>
2022-12-26 13:18:47 +0100

    Update BlogPostSidebar.vue

--- components/blog/post/BlogPostSidebar.vue
+++ components/blog/post/BlogPostSidebar.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="w-[360px] ml-6 p-6 top-28 sticky flex-shrink-0">
-    <BlogPostSidebarHeading>Contents</BlogPostSidebarHeading>
+  <div class="ml-6 flex-shrink-0">
+    <div class="w-[360px] p-6 top-24 sticky">
+      <BlogPostSidebarHeading>Contents</BlogPostSidebarHeading>
 
-    <BlogPostSidebarItem class="pl-2 border-l-4 border-white">Introduction</BlogPostSidebarItem>
-    <BlogPostSidebarItem>Community Guidelines Enforcement</BlogPostSidebarItem>
-    <BlogPostSidebarItem>Reports</BlogPostSidebarItem>
+      <BlogPostSidebarItem class="pl-2 border-l-4 border-white">Introduction</BlogPostSidebarItem>
+      <BlogPostSidebarItem>Community Guidelines Enforcement</BlogPostSidebarItem>
+      <BlogPostSidebarItem>Reports</BlogPostSidebarItem>
+    </div>
   </div>
 </template>
