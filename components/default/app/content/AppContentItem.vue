fedee73fbbb3d46e86b65c14b858e66cf9932215
StylexTV <stylextv42@gmail.com>
2022-12-27 16:56:54 +0100

    Update AppContentItem.vue

--- components/default/app/content/AppContentItem.vue
+++ components/default/app/content/AppContentItem.vue
@@ -1,5 +1,18 @@
 <template>
-  <div class="px-80">
+  <div class="px-80 opacity-0 translate-y-20 transition duration-700" :class="{'opacity-100 translate-y-0': show }">
     <slot></slot>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      show: false
+    }
+  },
+  mounted() {
+    this.show = true
+  }
+}
+</script>
