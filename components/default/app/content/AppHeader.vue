f4e26b479c566ff5d7f99d81bdf0c4a9f78ff26b
StylexTV <stylextv42@gmail.com>
2022-12-28 18:43:35 +0100

    Update AppHeader.vue

--- components/default/app/content/AppHeader.vue
+++ components/default/app/content/AppHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <AppContentItem class="w-full h-20 z-30 top-0 absolute flex justify-between">
+  <AppContentItem class="w-full h-20 flex justify-between">
     <AppHeaderLogo/>
 
     <AppHeaderNavbar>
