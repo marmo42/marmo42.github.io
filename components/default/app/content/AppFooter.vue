e582c55dc601b9e994244a856240bbb27f499e81
StylexTV <stylextv42@gmail.com>
2022-12-29 09:26:44 +0100

    Update AppFooter.vue

--- components/default/app/content/AppFooter.vue
+++ components/default/app/content/AppFooter.vue
@@ -1,4 +1,6 @@
 <template>
+  <hr class="w-full mt-[72px] border-[#272727]">
+
   <AppContentItem class="w-full h-24 flex items-center">
     <AppFooterNavbar>
       <AppFooterNavbarItem>GitHub</AppFooterNavbarItem>
