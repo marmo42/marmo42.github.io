f8c5f6152feec491d1bfcd4188c40f64afed8877
StylexTV <stylextv42@gmail.com>
2022-12-30 14:13:47 +0100

    Update ErrorHeroButton.vue

--- components/error/content/ErrorHeroButton.vue
+++ components/error/content/ErrorHeroButton.vue
@@ -1,3 +1,3 @@
 <template>
-  <Button class="h-9 !rounded-full bg-white !font-roboto !font-medium text-gray-900 text-sm">Go back home</Button>
+  <Button class="bg-white text-black text-sm">Go back home</Button>
 </template>
