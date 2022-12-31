fa8ac0f69bb1f5702c67537f082fcb2e22c59a24
StylexTV <stylextv42@gmail.com>
2022-12-31 11:07:02 +0100

    Update BlogPostListItemCover.vue

--- components/blog/list/item/BlogPostListItemCover.vue
+++ components/blog/list/item/BlogPostListItemCover.vue
@@ -1,5 +1,5 @@
 <template>
-  <img :src="'/content' + post._path + '/cover.jpg'" class="w-full mb-4 !rounded-md"/>
+  <img :src="'/content' + post._path + '/cover.jpg'" class="w-full mb-8 !rounded-md"/>
 </template>
 
 <script>
