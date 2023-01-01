f01391e639b582b81cbd85e91c8df883c0aed17b
StylexTV <stylextv42@gmail.com>
2023-01-01 12:50:41 +0100

    Added emphasis to start hero title

--- components/start/StartHero.vue
+++ components/start/StartHero.vue
@@ -2,7 +2,7 @@
   <StartHeroBackground/>
 
   <AppContentItem class="w-full mt-32 flex flex-col items-center">
-    <StartHeroTitle>Supercharged</StartHeroTitle>
+    <StartHeroTitle emphasized>Supercharged</StartHeroTitle>
     <StartHeroTitle>GitHub Experience</StartHeroTitle>
     <StartHeroSubtitle>Work faster with automated issue boards, a re-imagined inbox and command menu. A live collaboration platform packed into a desktop and web application.</StartHeroSubtitle>
   </AppContentItem>
--- components/start/hero/StartHeroTitle.vue
+++ components/start/hero/StartHeroTitle.vue
@@ -1,5 +1,13 @@
 <template>
-  <h1 class="!text-8xl !tracking-normal">
+  <h1 class="!text-8xl !tracking-normal" :class="{ 'italic !text-transparent bg-gradient-to-r from-blue-500 via-purple to-pink bg-clip-text': emphasized }">
     <slot></slot>
   </h1>
 </template>
+
+<script>
+export default {
+  props: {
+    emphasized: Boolean
+  }
+}
+</script>
