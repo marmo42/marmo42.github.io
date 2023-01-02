93bbc594fb2b2cc56a08d19f5d1e061f9ee3fdf8
StylexTV <stylextv42@gmail.com>
2023-01-02 17:40:41 +0100

    Update DesignHero.vue

--- components/content/design/DesignHero.vue
+++ components/content/design/DesignHero.vue
@@ -1,5 +1,5 @@
 <template>
-  <AppContentItem class="w-full flex flex-col gap-y-5">
+  <AppContentItem class="w-full mb-8 flex flex-col gap-y-5">
     <h1>Design Kit</h1>
     <DesignHeroText>Welcome to Nuxt design definition page. Our identity was redefined by conscientiously handpicking colors, typography and shapes in order to put forward how performant, useful & easy Nuxt products are.</DesignHeroText>
   </AppContentItem>
