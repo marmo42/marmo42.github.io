797cc5df3b4aa6a19d2357dad034d85b6171bd1a
StylexTV <stylextv42@gmail.com>
2023-01-02 18:11:41 +0100

    Updated start hero

--- assets/css/plugins/fonts.js
+++ assets/css/plugins/fonts.js
@@ -16,14 +16,8 @@ module.exports = plugin(function({ addBase }) {
       {
         fontFamily: 'YouTube Sans',
         fontStyle: 'normal',
-        fontWeight: 600,
-        src: 'url("./assets/fonts/youtube-sans/semi_bold.otf") format("opentype")'
-      },
-      {
-        fontFamily: 'YouTube Sans',
-        fontStyle: 'normal',
         fontWeight: 700,
-        src: 'url("./assets/fonts/youtube-sans/bold.otf") format("opentype")'
+        src: 'url("./assets/fonts/youtube-sans/bold.ttf") format("truetype")'
       },
       {
         fontFamily: 'Consolas',
