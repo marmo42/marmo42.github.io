515bbc8e4caaa8ba9bfb4bfc201b50b9e4c4939e
StylexTV <stylextv42@gmail.com>
2023-01-03 17:53:15 +0100

    Update DesignHero.vue

--- components/content/design/DesignHero.vue
+++ components/content/design/DesignHero.vue
@@ -1,6 +1,6 @@
 <template>
   <AppContentItem class="w-full mb-8 flex flex-col gap-y-5">
     <h1>Design</h1>
-    <DesignHeroText>Welcome to the design page where I explain some of the thoughts behind my brand design as well as the design of this particular website.</DesignHeroText>
+    <DesignHeroText>Welcome to the design page where I explain some of the thoughts behind my logo design as well as the design of this particular website.</DesignHeroText>
   </AppContentItem>
 </template>
