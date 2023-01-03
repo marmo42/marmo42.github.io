54bde30fcc99350934a26e24948b66f37bf59b1c
StylexTV <stylextv42@gmail.com>
2023-01-03 15:38:56 +0100

    Updated app footer

--- components/content/default/app/content/footer/navbar/AppFooterNavbarItem.vue
+++ components/content/default/app/content/footer/navbar/AppFooterNavbarItem.vue
@@ -1,5 +1,5 @@
 <template>
-  <NuxtLink :to="linkTo" class="font-inter font-normal text-sm text-gray-400">
+  <NuxtLink :to="linkTo" class="font-inter font-normal text-sm text-gray-400 hover:text-white-50">
     <slot></slot>
   </NuxtLink>
 </template>
--- components/content/start/StartHero.vue
+++ components/content/start/StartHero.vue
@@ -6,6 +6,6 @@
     <StartHeroTitle>GitHub Experience</StartHeroTitle>
     <StartHeroSubtitle>Work faster with automated issue boards, a re-imagined inbox and command menu. A live collaboration platform packed into a desktop and web application.</StartHeroSubtitle>
     <StartHeroButton/>
-    <StartHeroDescription>Focused on <b>speed</b> and <b>real-time collaboration</b> for working at a fast pace.</StartHeroDescription>
+    <StartHeroDescription>Focused on <b>software</b> and <b>game development</b> for front- and backend.</StartHeroDescription>
   </AppContentItem>
 </template>
