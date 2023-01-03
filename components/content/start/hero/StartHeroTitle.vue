98cba4c169c1f7c11683e9ef567636727af15d92
StylexTV <stylextv42@gmail.com>
2023-01-03 09:32:25 +0100

    Update StartHeroTitle.vue

--- components/content/start/hero/StartHeroTitle.vue
+++ components/content/start/hero/StartHeroTitle.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1 class="px-4 !font-inter !font-extrabold !text-8xl !tracking-normal" :class="{ 'italic !text-transparent bg-gradient-to-r from-blue-500 via-purple-500 to-red-500 bg-clip-text': emphasized }">
+  <h1 class="-m-4 p-4 !font-inter !font-extrabold !text-8xl !tracking-normal" :class="{ 'italic !text-transparent bg-gradient-to-r from-blue-500 via-purple-500 to-red-500 bg-clip-text': emphasized }">
     <slot></slot>
   </h1>
 </template>
