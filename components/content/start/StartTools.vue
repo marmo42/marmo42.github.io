e74896262067c14a7eca36c729f9b61733762df1
StylexTV <stylextv42@gmail.com>
2023-01-03 19:16:55 +0100

    Updated tools list

--- components/content/start/StartTools.vue
+++ components/content/start/StartTools.vue
@@ -3,10 +3,11 @@
     <h2>Experienced with numerous languages and tools</h2>
 
     <StartToolsList>
-      <StartToolsListItem tool="unjs" linkTo="https://unjs.io"/>
-      <StartToolsListItem tool="unjs" linkTo="https://unjs.io"/>
-      <StartToolsListItem tool="unjs" linkTo="https://unjs.io"/>
-      <StartToolsListItem tool="unjs" linkTo="https://unjs.io"/>
+      <StartToolsListItem tool="unity" linkTo="https://unity.com"/>
+      <StartToolsListItem tool="nuxt" linkTo="https://nuxt.com"/>
+      <StartToolsListItem tool="vuejs" linkTo="https://vuejs.org"/>
+      <StartToolsListItem tool="tailwindcss" linkTo="https://tailwindcss.com"/>
+      <StartToolsListItem tool="javascript" linkTo="https://www.javascript.com"/>
     </StartToolsList>
   </AppContentItem>
 </template>
