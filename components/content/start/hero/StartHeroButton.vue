218af5318d5baf5482c4df284b2d9c4df83909af
StylexTV <stylextv42@gmail.com>
2023-01-03 10:09:25 +0100

    Update StartHeroButton.vue

--- components/content/start/hero/StartHeroButton.vue
+++ components/content/start/hero/StartHeroButton.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="mt-12 p-[2px] rounded-lg bg-gradient-to-tr from-blue-500 to-red-500">
-    <Button class="bg-gray-800 text-white">
+  <NuxtLink to="/" class="mt-12 p-[2px] rounded-lg bg-gradient-to-tr from-blue-500 to-red-500">
+    <Button class="transition bg-gray-800 hover:bg-opacity-80 text-white">
       <StartHeroButtonIcon/>
 
       Continue with GitHub
     </Button>
-  </div>
+  </NuxtLink>
 </template>
