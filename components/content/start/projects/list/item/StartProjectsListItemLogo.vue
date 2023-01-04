00845f4ca3199d900ef091978f6353b74d4ea8f2
StylexTV <stylextv42@gmail.com>
2023-01-04 09:47:15 +0100

    Updated project list again

--- components/content/start/projects/list/item/StartProjectsListItemLogo.vue
+++ components/content/start/projects/list/item/StartProjectsListItemLogo.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="p-4 flex items-center justify-center col-span-5">
+  <div class="py-16 lg:p-4 flex items-center justify-center col-span-5">
     <Image page="start" :path="path" class="w-20 h-20 rounded-lg ring-1 ring-gray-750"/>
   </div>
 </template>
--- components/content/start/projects/list/item/StartProjectsListItemText.vue
+++ components/content/start/projects/list/item/StartProjectsListItemText.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="pl-12 py-16 col-span-7">
+  <div class="px-12 pb-12 lg:pr-0 lg:py-16 col-span-7">
     <slot></slot>
   </div>
 </template>
