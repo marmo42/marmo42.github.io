fdc55decb4ca0d81e96df46f0d310dae75ee7852
StylexTV <stylextv42@gmail.com>
2023-01-04 13:40:34 +0100

    Updated example post

--- components/content/start/StartHero.vue
+++ components/content/start/StartHero.vue
@@ -3,7 +3,7 @@
 
   <AppContentItem class="w-full mt-32 mb-20 flex flex-col items-center">
     <StartHeroTitle emphasized>Overloaded</StartHeroTitle>
-    <StartHeroTitle>Stuff Creator</StartHeroTitle>
+    <StartHeroTitle>Stuff Developer</StartHeroTitle>
     <StartHeroSubtitle>Hello there, and thanks for visiting my personal portfolio page. I am a mainly Java oriented software developer who loves to create things.</StartHeroSubtitle>
     <StartHeroButton/>
     <StartHeroDescription>Focused on <b>software</b> and <b>game development</b> for front- and backend.</StartHeroDescription>
