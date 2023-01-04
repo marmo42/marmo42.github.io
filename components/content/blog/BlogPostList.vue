55c5848cdf42be50b1615a56fb8107cdd054f3fa
StylexTV <stylextv42@gmail.com>
2023-01-04 08:27:01 +0100

    Update BlogPostList.vue

--- components/content/blog/BlogPostList.vue
+++ components/content/blog/BlogPostList.vue
@@ -2,7 +2,7 @@
   <AppContentItem class="w-full">
     <BlogPostListTitle>{{ title }}</BlogPostListTitle>
 
-    <div class="grid grid-cols-2 gap-12">
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-12">
       <slot></slot>
     </div>
   </AppContentItem>
