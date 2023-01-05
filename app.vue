1db5f8f8778133d68109e3756fd23f01f7c1405c
StylexTV <stylextv42@gmail.com>
2023-01-05 12:18:46 +0100

    Update app.vue

--- app.vue
+++ app.vue
@@ -14,15 +14,9 @@
 </template>
 
 <script setup>
-import Rellax from 'rellax';
-
 useHead({
   titleTemplate: (titleChunk) => {
     return titleChunk ? `${titleChunk} | StylexTV` : 'StylexTV';
   }
 });
-
-onMounted(() => {
-  new Rellax('.rellax');
-})
 </script>
